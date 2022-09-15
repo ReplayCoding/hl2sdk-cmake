c3ead8b54ff3638b0f4600f744160d7c54ea0b3d
ReplayCoding <replaycoding@gmail.com>
2022-09-15 14:41:21 -0700

    this is a cmake repo now :|

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           buildInputs = with pkgs; [];
           nativeBuildInputs = with pkgs; [
             # For build
-            meson
+            cmake
             ninja
             # Debugging
             lldb
