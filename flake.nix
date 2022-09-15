90cac2c40adc63ff2a3f790825f14fea9defa9a7
ReplayCoding <replaycoding@gmail.com>
2022-09-15 14:41:16 -0700

    cleanup

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    devShells = forSystems (
+      system: let
+        pkgs_ = import nixpkgs {
+          localSystem.system = system;
+          crossSystem.system = "i686-linux";
+        };
+        pkgs = pkgs_.__splicedPackages;
+      in {
+        default = pkgs.stdenv.mkDerivation {
+          name = "devshell";
+          # For systemd-coredump
+          SYSTEMD_DEBUGGER = "lldb";
+          hardeningDisable = [ "all" ];
+          buildInputs = with pkgs; [];
+          nativeBuildInputs = with pkgs; [
+            # For build
+            meson
+            ninja
+            # Debugging
+            lldb
+          ];
+        };
+      }
+    );
+  };
+}
